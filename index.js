228dd4aaf040ee528ebaee1b114dd6f77bd7aafc
Pablo Frank <jgfpablo@gmail.com>
2024-11-04 03:10:13 -0300

    cambios

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(cors());
-app.use(express.json());
+// app.use(express.json());
+
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 mongoose
     .connect(process.env.MONGO_URI)
