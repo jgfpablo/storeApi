75b402874c13f8f4d5b4c5979044ecda13dd531a
Pablo Frank <jgfpablo@gmail.com>
2024-11-09 10:45:09 -0300

    cambios

--- index.js
+++ index.js
@@ -8,17 +8,8 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-const cors = require("cors");
-const express = require("express");
-
-// Habilitar CORS para todos los orígenes
-app.use(
-    cors({
-        origin: "*", // Permite solicitudes de cualquier origen
-        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Permite los métodos HTTP que necesites
-        allowedHeaders: ["Content-Type", "Authorization"], // Permite los encabezados necesarios
-    })
-);
+app.use(cors());
+// app.use(express.json());
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
