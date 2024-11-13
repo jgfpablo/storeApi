ab76edf8bb54ffd6b3281bdb96d478c5dd5ce265
Pablo Frank <jgfpablo@gmail.com>
2024-11-13 17:39:21 -0300

    cambios

--- index.js
+++ index.js
@@ -8,8 +8,15 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-app.use(cors());
-// app.use(express.json());
+// app.use(cors());
+
+app.use(
+    cors({
+        origin: "*", // Permitir cualquier origen
+        methods: ["GET", "POST", "PUT", "DELETE"], // Métodos permitidos
+        allowedHeaders: ["Content-Type", "Authorization"], // Encabezados permitidos
+    })
+);
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
