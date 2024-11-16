398e4aca1ee7dcff988ef4af0156162b4cb9bc86
Pablo Frank <jgfpablo@gmail.com>
2024-11-16 06:13:40 -0300

    cambios

--- routes/filament.js
+++ routes/filament.js
@@ -55,4 +55,44 @@ router.get("/", async (req, res) => {
     }
 });
 
+router.put("/updateProduct", authenticateToken, async (req, res) => {
+    try {
+        // Obtener el nombre y los datos del producto desde el cuerpo de la solicitud
+        const { filament } = req.body; // Desestructurar el 'name' y 'product' del cuerpo de la solicitud
+
+        if (!filament.color) {
+            return res.status(400).json({
+                error: "El campo 'name' es obligatorio para actualizar el producto",
+            });
+        }
+
+        // Buscar el producto por nombre y actualizarlo con los datos proporcionados
+        const updateFilament = await Filament.findOneAndUpdate(
+            { filament: color }, // Condición de búsqueda: producto con el nombre especificado
+            filament, // Datos a actualizar (product contiene todos los campos nuevos)
+            { new: true } // Opción: retorna el producto actualizado
+        );
+
+        // Si no se encuentra el producto
+        if (!updateFilament) {
+            return res.status(404).json({ error: "Producto no encontrado" });
+        }
+
+        // Responder con el producto actualizado
+
+        res.json({
+            alert: "Filamento Actualizado exitosamente",
+            status: "success",
+            data: req.body.filament.color,
+        });
+    } catch (error) {
+        // Manejo de errores
+        res.status(500).json({
+            error: `Error al actualizar el producto: ${error.message}`,
+            status: "failure",
+            data: req.body.filament.color,
+        });
+    }
+});
+
 module.exports = router;
